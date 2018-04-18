1ea757a02c4628c326aee205c42091467d1d55e9
Aaron <aaroneg@users.noreply.github.com>
2018-04-18 16:25:51 -0500

    Create initializeEnvironment.sh

---
+++ initializeEnvironment.sh
@@ -0,0 +1,16 @@
+echo "export DISPLAY=:0.0" >> ~/.bashrc
+echo "setenv DISPLAY :0.0" >> ~/.cshrc
+sudo apt-get update
+sudo apt-get install -y tcsh xfonts-base python-qt4       \
+                        gsl-bin netpbm gnome-tweak-tool   \
+                        libjpeg62 xvfb xterm vim curl     \
+                        gedit evince                      \
+                        libglu1-mesa-dev libglw1-mesa     \
+                        libxm4 build-essential            \
+                        gnome-terminal nautilus           \
+                        gnome-icon-theme-symbolic         \
+                        libcurl4-gnutls-dev
+cd
+curl -O https://afni.nimh.nih.gov/pub/dist/bin/linux_ubuntu_16_64/@update.afni.binaries
+tcsh @update.afni.binaries -package linux_ubuntu_16_64  -do_extras
+sudo mkdir /mnt/labs
