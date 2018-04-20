5314507edc5dcd5ad5c01e2d6a4ff91e20f5e1be
Aaron <aaroneg@users.noreply.github.com>
2018-04-20 10:08:47 -0500

    Create niceifyTerminal.sh

---
+++ niceifyTerminal.sh
@@ -0,0 +1,8 @@
+echo 'set filec'    >> ~/.cshrc
+echo 'set autolist' >> ~/.cshrc
+echo 'set nobeep'   >> ~/.cshrc
+
+echo 'alias ls ls --color=auto' >> ~/.cshrc
+echo 'alias ll ls --color -l'   >> ~/.cshrc
+echo 'alias ls="ls --color"'    >> ~/.bashrc
+echo 'alias ll="ls --color -l"' >> ~/.bashrc
